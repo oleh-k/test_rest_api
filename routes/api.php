aa897e308701a87e880db0379b36b48580b765e0
oleh_ok <oleh.k@hotmail.com>
2023-05-28 18:37:06 +0300

    setup sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/products', ProductController::class);
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::resource('/products', ProductController::class);
 });
+
+
